f38e93bfc6972fdebe69b837263bb1f74d505043
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-02-24 19:30:53 -0800

    Add next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+const plugins = [withBundleAnalyzer];
+
+const composePlugins = (phase, defaultConfig) =>
+  plugins.reduce(
+    (acc, plugin) => {
+      const newConfig = plugin(acc);
+
+      return typeof newConfig === 'function'
+        ? newConfig(phase, defaultConfig)
+        : newConfig;
+    },
+    { ...nextConfig }
+  );
+
+module.exports = composePlugins;
