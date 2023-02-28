c84ccc9e96c9fe8983f77debd5d3c302b04632e6
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-02-28 11:53:59 -0800

    Install inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { PageLayout } from '~/components/layout/PageLayout';
 import { DefaultSEO } from '~/components/seo';
 
+import '@fontsource/inter/variable-full.css';
 import '~/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
