93bd8bdf75ac3d869266a07549ee50958558c42b
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-02 15:24:10 -0800

    Add content to individual tag page

--- src/pages/tags/[tag].tsx
+++ src/pages/tags/[tag].tsx
@@ -1,6 +1,35 @@
+import Balancer from 'react-wrap-balancer';
+
+import { Heading } from '~/components/Heading';
+import { BlogPostList } from '~/components/home/BlogPostList';
+import { Paragraph } from '~/components/Paragraph';
 import { SEO } from '~/components/seo';
+import { Spacer } from '~/components/Spacer';
+
+const posts = [
+  {
+    slug: '/blog/asdf',
+    date: '2022-12-15T15:00:00.000Z',
+    title: 'Some blog post title',
+    summary:
+      'This is a blog post summary. We will be going over some sort of topic.',
+    tags: ['react', 'next-js'],
+  },
+  {
+    slug: '/blog/ghjk',
+    date: '2022-12-15T15:00:00.000Z',
+    title: 'JavaScript is really cool',
+    summary:
+      'I bet you did not know that JavaScript is the coolest. In this post, we will discuss it. This is an extra long description because I need to see what that looks like.',
+    tags: ['javascript'],
+  },
+];
+
+const tag = 'react';
 
 export default function Tag() {
+  const count = posts.length;
+
   return (
     <>
       <SEO
@@ -8,7 +37,20 @@ export default function Tag() {
         title="React posts"
         description="Content focusing on React, JavaScript, Node.js, and more."
       />
-      <div>Tag page</div>
+
+      <Spacer size="8" />
+
+      <header>
+        <Paragraph lead>
+          <Balancer>{`${count} posts tagged:`}</Balancer>
+        </Paragraph>
+
+        <Heading>{tag}</Heading>
+      </header>
+
+      <Spacer size="8" />
+
+      <BlogPostList posts={posts} />
     </>
   );
 }
