ea1540ba3fd3e1e46f6caacc63d62846ab0ea250
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-02 09:57:52 -0800

    Create reusable page intro component

---
+++ src/components/PageIntro.tsx
@@ -0,0 +1,32 @@
+import Balancer from 'react-wrap-balancer';
+
+import { Heading } from '~/components/Heading';
+import { Paragraph } from '~/components/Paragraph';
+import { Spacer } from '~/components/Spacer';
+
+type Props = {
+  title: string;
+  description?: string;
+};
+
+export function PageIntro({ title, description }: Props) {
+  return (
+    <>
+      <Spacer size="16" />
+
+      <section>
+        <header>
+          <Heading>{title}</Heading>
+
+          {description ? (
+            <Paragraph lead>
+              <Balancer>{description}</Balancer>
+            </Paragraph>
+          ) : null}
+        </header>
+      </section>
+
+      <Spacer size="16" />
+    </>
+  );
+}
