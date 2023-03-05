438b3fdd82a566904ca4c602a03747f403c9e3d0
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-05 12:35:56 -0800

    Make sure post list follows same unordered list structure

--- src/components/blog/PostList.tsx
+++ src/components/blog/PostList.tsx
@@ -19,41 +19,40 @@ export function PostList({ posts }: Props) {
   }
 
   return (
-    <>
+    <ul>
       {posts.map(({ slug, title, summary, date, tags }) => (
-        <article
-          key={slug}
-          className="flex flex-col justify-between gap-4 border-t py-6 md:flex-row"
-        >
-          <div>
-            <Heading size="sm" as="h3" className="mb-2">
-              {title}
-            </Heading>
+        <li key={slug}>
+          <article className="flex flex-col justify-between gap-4 border-t py-6 md:flex-row">
+            <div>
+              <Heading size="sm" as="h3" className="mb-2">
+                {title}
+              </Heading>
 
-            <Paragraph>{summary}</Paragraph>
+              <Paragraph>{summary}</Paragraph>
 
-            <Link
-              href={slug}
-              className="font-medium text-primary-700 transition-colors hover:text-primary-800"
-            >
-              Read more <span className="sr-only">, {title}</span>
-              <span aria-hidden="true">&rarr;</span>
-            </Link>
-          </div>
+              <Link
+                href={slug}
+                className="font-medium text-primary-700 transition-colors hover:text-primary-800"
+              >
+                Read more <span className="sr-only">, {title}</span>
+                <span aria-hidden="true">&rarr;</span>
+              </Link>
+            </div>
 
-          <dl className="mt-4 flex flex-col gap-2 md:mt-0 md:items-end">
-            <dt className="sr-only">Published on:</dt>
-            <dd className="whitespace-nowrap text-base leading-6 text-gray-500 dark:text-gray-400">
-              <time dateTime={date}>December 27, 2022</time>
-            </dd>
+            <dl className="mt-4 flex flex-col gap-2 md:mt-0 md:items-end">
+              <dt className="sr-only">Published on:</dt>
+              <dd className="whitespace-nowrap text-base leading-6 text-gray-500 dark:text-gray-400">
+                <time dateTime={date}>December 27, 2022</time>
+              </dd>
 
-            <dt className="sr-only">Related tags:</dt>
-            <dd>
-              <TagsList className="md:justify-end" compact tags={tags} />
-            </dd>
-          </dl>
-        </article>
+              <dt className="sr-only">Related tags:</dt>
+              <dd>
+                <TagsList className="md:justify-end" compact tags={tags} />
+              </dd>
+            </dl>
+          </article>
+        </li>
       ))}
-    </>
+    </ul>
   );
 }
