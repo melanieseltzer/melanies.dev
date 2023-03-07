9feef0f45e65af961a48d09e6606c4bed51000e4
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-07 15:22:35 -0800

    Fancy css terminal window (#16)
    * Create a decorative animated terminal window for hero section
    
    * Tweak styles and simplify html
    
    * Add border and dark mode bg
    
    * clsx isn't needed anymore
    
    * Style updates
    
    * Tweak hero layout
    
    * Fix section announcement on voiceover
    
    * Rename component

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -5,31 +5,39 @@ import { Heading } from '~/components/Heading';
 import { Paragraph } from '~/components/Paragraph';
 import { Spacer } from '~/components/Spacer';
 
+import { TerminalWindow } from './TerminalWindow';
+
 export function HeroSection() {
   return (
-    <section aria-labelledby="hero-title">
-      <header>
-        <Heading
-          id="hero-title"
-          className="text-5xl tracking-tight md:text-6xl lg:text-7xl"
-        >
+    <section
+      aria-label="Hero intro"
+      className="mt-8 mb-20 flex flex-col justify-between gap-4 lg:my-28 lg:flex-row lg:items-center lg:gap-10"
+    >
+      <div className="lg:max-w-lg">
+        <Heading className="text-5xl tracking-tight md:text-6xl lg:text-7xl">
           <Balancer>
             Crafting <GradientText>delightful experiences</GradientText> for the
             web
           </Balancer>
         </Heading>
-      </header>
 
-      <Spacer size="4" />
+        <Spacer size="4" />
 
-      <Paragraph lead>
-        <Balancer>
-          Hello 👋 I&apos;m{' '}
-          <GradientText className="font-semibold">Melanie Seltzer</GradientText>
-          , a Software Engineer based in Portland, Oregon, making stuff and
-          things with JavaScript. Welcome to my little digital home.
-        </Balancer>
-      </Paragraph>
+        <Paragraph lead>
+          <Balancer>
+            Hello 👋 I&apos;m{' '}
+            <GradientText className="font-semibold">
+              Melanie Seltzer
+            </GradientText>
+            , a Software Engineer based in Portland, Oregon, making stuff and
+            things with JavaScript. Welcome to my little digital home.
+          </Balancer>
+        </Paragraph>
+      </div>
+
+      <div className="max-w-md flex-auto">
+        <TerminalWindow />
+      </div>
     </section>
   );
 }
---
+++ src/components/home/TerminalWindow.tsx
@@ -0,0 +1,44 @@
+import { Spacer } from '~/components/Spacer';
+
+export function TerminalWindow() {
+  return (
+    <div
+      role="img"
+      aria-label="Illustration of my terminal window which uses Sindre Sorhus' Pure prompt theme"
+      className="h-72 w-full max-w-md rounded-lg border bg-gray-900 p-4 font-mono text-xl font-semibold shadow-2xl shadow-secondary-500/50 dark:bg-gray-800"
+    >
+      <div className="flex gap-2 pb-4">
+        <WindowButton color="red" />
+        <WindowButton color="yellow" />
+        <WindowButton color="green" />
+      </div>
+
+      <Spacer size="2" />
+
+      <div className="text-sky-500">
+        {/* tilde slash */}
+        &#126;/melanies.dev
+      </div>
+
+      <Spacer size="2" />
+
+      <div className="flex cursor-text gap-2 text-2xl text-primary-500">
+        {/* arrow prompt */}
+        &#10095;
+        {/* blinking cursor */}
+        {/* only blink if reduce motion is off */}
+        <div className="m-1 w-[12px] items-stretch bg-gray-300 motion-safe:animate-[cursorBlink_1.5s_steps(2)_infinite]" />
+      </div>
+    </div>
+  );
+}
+
+const colorMap: Record<string, string> = {
+  red: 'bg-red-500',
+  yellow: 'bg-yellow-500',
+  green: 'bg-green-500',
+};
+
+function WindowButton({ color }: { color: string }) {
+  return <span className={`h-3 w-3 rounded-full ${colorMap[color]}`} />;
+}
