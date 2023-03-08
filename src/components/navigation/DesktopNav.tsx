8b2f8f2501b9e10ac4b2c688663b5c40f1703b9e
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-08 14:16:52 -0800

    Nav styling (#18)
    * Work on the desktop nav styling
    
    * Improve the mobile nav experience
    
    * Icon size

--- src/components/navigation/DesktopNav.tsx
+++ src/components/navigation/DesktopNav.tsx
@@ -5,10 +5,13 @@ import { routes } from './routes';
 export function DesktopNav() {
   return (
     <nav className="hidden sm:block">
-      <ul className="flex">
+      <ul className="flex space-x-4">
         {routes.map(route => (
           <li key={route.title}>
-            <Link href={route.href} className="p-4">
+            <Link
+              href={route.href}
+              className="rounded-md px-3 py-2 text-base font-medium text-gray-600 transition-colors duration-300 hover:bg-neutral-100 hover:text-gray-900"
+            >
               {route.title}
             </Link>
           </li>
