c3ea8a62d36547f8d76b79b73b82060c83e1da58
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-09 14:13:55 -0800

    Work on the styling and layout for the about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,7 @@
+import { Image } from '~/components/Image';
+import { PageIntro } from '~/components/PageIntro';
 import { SEO } from '~/components/seo';
+import { TechStack } from '~/components/TechStack';
 
 export default function About() {
   return (
@@ -7,7 +10,76 @@ export default function About() {
         title="About"
         description="Software engineer and perpetual tinkerer specializing in front-end JavaScript development."
       />
-      <div>About page</div>
+
+      <PageIntro
+        heading="About Melanie Seltzer"
+        subheading="Software engineer and perpetual tinkerer specializing in front-end JavaScript development."
+      />
+
+      <div className="flex flex-wrap justify-between gap-4">
+        <div className="mb-12">
+          <div className="sticky top-28 flex flex-col items-center gap-8">
+            <Image
+              className="rounded-full p-1 ring-4 ring-secondary-600"
+              src="/images/avatar.jpg"
+              alt="Portrait photo of Melanie"
+              width={192}
+              height={192}
+            />
+
+            <TechStack
+              size="lg"
+              label="Current fave tech/tools:"
+              showLabel
+              tech={[
+                'React',
+                'TypeScript',
+                'Next.js',
+                'Tailwind CSS',
+                'VSCode',
+              ]}
+            />
+          </div>
+        </div>
+
+        {/* eslint-disable */}
+        <div className="prose prose-lg prose-slate">
+          <h2>Hey there 👋 I'm Melanie</h2>
+          <p>
+            Tailwind removes all of the default browser styling from paragraphs,
+            headings, lists and more. This ends up being really useful for
+            building application UIs because you spend less time undoing
+            user-agent styles, but when you <em>really are</em> just trying to
+            style some content that came from a rich-text editor in a CMS or a
+            markdown file, it can be surprising and unintuitive.
+          </p>
+
+          <h2>Tech stack</h2>
+
+          <ul>
+            <li>So here is the first item in this list.</li>
+            <li>In this example we're keeping the items short.</li>
+            <li>Later, we'll use longer, more complex list items.</li>
+          </ul>
+          <h2>Summary</h2>
+          <p>
+            Tailwind removes all of the default browser styling from paragraphs,
+            headings, lists and more. This ends up being really useful for
+            building application UIs because you spend less time undoing
+            user-agent styles, but when you <em>really are</em> just trying to
+            style some content that came from a rich-text editor in a CMS or a
+            markdown file, it can be surprising and unintuitive.
+          </p>
+
+          <h2>Tech stack</h2>
+
+          <ul>
+            <li>So here is the first item in this list.</li>
+            <li>In this example we're keeping the items short.</li>
+            <li>Later, we'll use longer, more complex list items.</li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
