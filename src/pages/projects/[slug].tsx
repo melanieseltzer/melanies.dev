90b42015621aab404f369c62f6ed02f389efd908
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-08 19:30:27 -0800

    Fine tune the prose layout for projects and blog posts

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,6 +1,5 @@
 import { RxDividerVertical as Separator } from 'react-icons/rx';
 
-import { PageIntro } from '~/components/PageIntro';
 import { DemoButton } from '~/components/projects/DemoButton';
 import { SourceCodeButton } from '~/components/projects/SourceCodeButton';
 import { SEO } from '~/components/seo';
@@ -9,7 +8,7 @@ import { TechStack } from '~/components/TechStack';
 
 export default function Project() {
   return (
-    <div className="mx-auto max-w-3xl">
+    <>
       <SEO
         titleTemplate="%s"
         // TODO: replace mocked content
@@ -17,46 +16,55 @@ export default function Project() {
         description="This is an example project summary."
       />
 
-      <PageIntro
-        compact
-        heading="This is an example project title that is really long and wraps"
-        subheading="This is an example project summary"
-      />
-
-      <div className="-mt-8 flex flex-wrap items-center gap-4">
-        <TechStack tech={['React', 'TypeScript', 'Next.js', 'CSS']} />
-
-        <Separator aria-hidden="true" size={15} className="text-gray-400" />
-
-        <div className="flex flex-wrap gap-2">
-          <DemoButton href="https://test.com" />
-
-          <SourceCodeButton href="https://github.com/melanieseltzer" />
-        </div>
-      </div>
-
       <Spacer size="16" />
 
-      {/* eslint-disable */}
-      <div className="prose prose-lg prose-slate">
-        <h2>Summary</h2>
-        <p>
-          Tailwind removes all of the default browser styling from paragraphs,
-          headings, lists and more. This ends up being really useful for
-          building application UIs because you spend less time undoing
-          user-agent styles, but when you <em>really are</em> just trying to
-          style some content that came from a rich-text editor in a CMS or a
-          markdown file, it can be surprising and unintuitive.
-        </p>
+      <div className="prose prose-lg prose-slate mx-auto">
+        <article>
+          <h1>
+            This is an example project title that is really long and wraps
+          </h1>
 
-        <h2>Tech stack</h2>
+          <p className="lead">
+            Until now, trying to style an article, document, or blog post with
+            Tailwind has been a tedious task that required a keen eye for
+            typography and a lot of complex custom CSS.
+          </p>
 
-        <ul>
-          <li>So here is the first item in this list.</li>
-          <li>In this example we're keeping the items short.</li>
-          <li>Later, we'll use longer, more complex list items.</li>
-        </ul>
+          <Spacer size="4" />
+
+          <div className="not-prose flex flex-wrap items-center gap-4">
+            <div className="flex flex-wrap gap-2">
+              <DemoButton href="https://test.com" />
+
+              <SourceCodeButton href="https://github.com/melanieseltzer" />
+            </div>
+
+            <Separator aria-hidden="true" size={15} className="text-gray-400" />
+            <TechStack tech={['React', 'TypeScript', 'Next.js', 'CSS']} />
+          </div>
+
+          <hr />
+
+          {/* eslint-disable */}
+          <h2>Summary</h2>
+          <p>
+            Tailwind removes all of the default browser styling from paragraphs,
+            headings, lists and more. This ends up being really useful for
+            building application UIs because you spend less time undoing
+            user-agent styles, but when you <em>really are</em> just trying to
+            style some content that came from a rich-text editor in a CMS or a
+            markdown file, it can be surprising and unintuitive.
+          </p>
+
+          <h2>Tech stack</h2>
+
+          <ul>
+            <li>So here is the first item in this list.</li>
+            <li>In this example we're keeping the items short.</li>
+            <li>Later, we'll use longer, more complex list items.</li>
+          </ul>
+        </article>
       </div>
-    </div>
+    </>
   );
 }
