c6addc46a1869663e6e9c50656c308ad7a0fbbc9
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-16 16:19:10 -0700

    Create page content type

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,6 +1,18 @@
-import { makeSource } from 'contentlayer/source-files';
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+export const Page = defineDocumentType(() => ({
+  name: 'Page',
+  filePathPattern: 'pages/**/*.mdx',
+  contentType: 'mdx',
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: page => page._raw.sourceFileName.replace(/\.mdx$/, ''),
+    },
+  },
+}));
 
 export default makeSource({
   contentDirPath: 'content',
-  documentTypes: [],
+  documentTypes: [Page],
 });
