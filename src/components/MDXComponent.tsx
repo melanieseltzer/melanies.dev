41ecf0cf5dd34b35cda53e78ed77ba1b5b2de54d
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-16 17:25:03 -0700

    Use custom link component for all links in mdx

--- src/components/MDXComponent.tsx
+++ src/components/MDXComponent.tsx
@@ -4,10 +4,7 @@ import { DocumentTypes } from 'contentlayer/generated';
 import type { MDXComponents } from 'mdx/types';
 
 import { Image } from '~/components/Image';
-
-const defaultMDXComponents: MDXComponents = {
-  Image,
-};
+import { Link } from '~/components/Link';
 
 interface MDXComponentProps {
   source: DocumentTypes;
@@ -20,9 +17,17 @@ export const MDXComponent = ({ source, components }: MDXComponentProps) => {
   return (
     <RenderMDXComponent
       components={{
-        ...defaultMDXComponents,
+        // This is an icky but essentially harmless type assertion due to conflicting `href` types.
+        // `href` will NEVER be undefined in our custom `Link` component (we've ensured that),
+        // but `MDXComponents` says it could be so it's conflicting.
+        ...(defaultMDXComponents as MDXComponents),
         ...components,
       }}
     />
   );
+};
+
+const defaultMDXComponents = {
+  Image,
+  a: Link,
 };
