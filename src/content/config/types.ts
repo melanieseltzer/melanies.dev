dd56a008f9187f80f993f8f3338f148991e573a0
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-21 22:10:46 -0700

    Refactor content types

--- src/content/config/types.ts
+++ src/content/config/types.ts
@@ -1,5 +1,7 @@
 import type { DocumentTypeDef } from 'contentlayer/source-files';
 
+export type { DocumentTypes } from 'contentlayer/generated';
+
 export type SchemaFieldDefs<T extends string> = {
   fields?: DocumentTypeDef<T>['fields'];
   computedFields?: DocumentTypeDef<T>['computedFields'];
