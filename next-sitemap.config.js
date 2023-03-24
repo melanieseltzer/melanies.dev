0edd2cb616e99408c7661fc35ef98b58cb2da0e6
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-24 16:24:09 -0700

    Modify priority

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,3 @@
-const HIGH_PRIORITY = ['/about', '/blog', '/projects'];
 const EXCLUDE = ['/blog/page/*', '/blog/tags/*'];
 
 /** @type {import('next-sitemap').IConfig} */
@@ -14,15 +13,22 @@ module.exports = {
   }),
 };
 
+const HIGH_PRIORITY = ['/blog', '/projects'];
+
 function getPriority(path) {
   const top = path === '/';
-  const high = HIGH_PRIORITY.includes(path);
+  const high =
+    HIGH_PRIORITY.includes(path) ||
+    // actual blog posts (note the trailing slash)
+    path.includes('/blog/') ||
+    // individual project page (note the trailing slash)
+    path.includes('/projects/');
 
   switch (true) {
     case top:
-      return 1;
+      return 1.0;
     case high:
-      return 0.8;
+      return 0.7;
     default:
       return 0.64;
   }
