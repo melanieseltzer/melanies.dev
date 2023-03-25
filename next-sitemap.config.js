36d9c08c93eb0032628981e6abc081f4d0c12c0c
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-24 17:03:10 -0700

    Add sitemap and robots.txt (#35)
    * Generate sitemap/robots.txt with next-sitemap
    
    * Remove lastmod from sitemap for now
    
    * Modify priority

---
+++ next-sitemap.config.js
@@ -0,0 +1,35 @@
+const EXCLUDE = ['/blog/page/*', '/blog/tags/*'];
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://melanies.dev',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  exclude: EXCLUDE,
+
+  transform: async (config, path) => ({
+    loc: path,
+    priority: getPriority(path),
+  }),
+};
+
+const HIGH_PRIORITY = ['/blog', '/projects'];
+
+function getPriority(path) {
+  const top = path === '/';
+  const high =
+    HIGH_PRIORITY.includes(path) ||
+    // actual blog posts (note the trailing slash)
+    path.includes('/blog/') ||
+    // individual project page (note the trailing slash)
+    path.includes('/projects/');
+
+  switch (true) {
+    case top:
+      return 1.0;
+    case high:
+      return 0.7;
+    default:
+      return 0.64;
+  }
+}
