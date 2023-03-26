38d847dfa5ddf03e6f0b527b0831696450640d4b
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-03-26 09:49:29 -0700

    Install the rehype plugins

--- src/content/config/plugins.ts
+++ src/content/config/plugins.ts
@@ -1,7 +1,13 @@
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypePresetMinify from 'rehype-preset-minify';
+import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
 import type { Pluggable } from 'unified';
 
 export const remarkPlugins: Pluggable[] = [remarkGfm];
 
-export const rehypePlugins: Pluggable[] = [rehypePresetMinify];
+export const rehypePlugins: Pluggable[] = [
+  rehypeSlug,
+  rehypeAutolinkHeadings,
+  rehypePresetMinify,
+];
