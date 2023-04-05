7795dced7bc8f98bef44a6aa46cbffbc5e6ef5dc
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-04-05 09:13:47 -0700

    Add progress bar indicator for route changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { Provider as ReactWrapBalancerProvider } from 'react-wrap-balancer';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import ProgressBar from 'nextjs-progressbar';
 
 import { PageLayout } from '~/components/layout/PageLayout';
 import { DefaultSEO } from '~/components/seo';
 
+import '~/styles/nprogress.css';
 import '@fontsource/inter/variable-full.css';
 import '~/styles/globals.css';
 import '@code-hike/mdx/dist/index.css';
@@ -13,6 +15,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSEO />
+
+      <ProgressBar
+        options={{ showSpinner: false }}
+        // Necessary to minify the CSS :/
+        // ref: https://github.com/apal21/nextjs-progressbar/pull/85
+        transformCSS={() => <></>}
+      />
 
       <ReactWrapBalancerProvider>
         <ThemeProvider attribute="class">
