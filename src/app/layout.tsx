861489298eead7b6bf2c216b09ff10a067e41677
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-08-11 17:26:56 -0700

    Port over icon/manifest metadata into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,35 @@
+import { Metadata } from 'next';
 import NextTopLoader from 'nextjs-toploader';
+
+import { siteMetadata } from '~/config/metadata';
 
 interface Props {
   children: React.ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: siteMetadata.metaTitle,
+  description: siteMetadata.metaDescription,
+  icons: {
+    icon: [
+      {
+        rel: 'mask-icon',
+        url: '/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
+  themeColor: '#fff',
+  other: {
+    'msapplication-TileColor': '#fff',
+  },
+};
 
 export default function RootLayout({ children }: Props) {
   return (
